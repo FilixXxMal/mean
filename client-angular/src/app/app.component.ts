442d1120a7c8a583a4919e47f9ea9cb2addd6316
FilixXxMal <maldifassi.filippo.maria@iisgalvanimi.edu.it>
2019-10-15 10:51:45 +0000

    commit 2-mean

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client-angular';
+export class AppComponent implements OnInit{
+  results: string[];
+  private BASE_URL:string = '/api';
+    // Inject HttpClient into your component or service.
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+      // Make the HTTP request:
+      this.http.get('https://8080-d915c72b-5e66-41a9-aff0-aedc687e2cfa.ws-eu0.gitpod.io/api').subscribe(data => {
+        // Read the result field from the JSON response.
+        this.results = data['results'];
+      });
+    }
 }
